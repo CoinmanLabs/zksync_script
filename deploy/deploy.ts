b1e487f5bfb43d0168448a07d104b3024439cd4f
zksync-web3 <132649428+zksync-web3@users.noreply.github.com>
2023-05-15 00:58:48 +0800

    initial
    initial

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -1,4 +1,4 @@
-import { Wallet, utils } from "zksync-web3";
+import { Wallet, utils } from "zksync-web3js";
 import * as ethers from "ethers";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { Deployer } from "@matterlabs/hardhat-zksync-deploy";
